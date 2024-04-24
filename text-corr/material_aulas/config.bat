b3dbce07d0c43d90a035afebdd11673a830d85b8
ProfCarmino <73600424+ProfCarmino@users.noreply.github.com>
2024-04-24 19:03:45 -0300

    material de apoio aulas

---
+++ text-corr/material_aulas/config.bat
@@ -0,0 +1,10 @@
+nest new text-corr
+cd text-corr
+npm install @nestjs/typeorm typeorm mysql2
+npm install @nestjs/jwt
+npm install @nestjs/passport passport passport-jwt bcryptjs
+npm install @types/bcryptjs @types/passport-jwt
+npm install @nestjs/config
+npm install class-validator
+npm i @nestjs/mapped-types
+npm install class-transformer
